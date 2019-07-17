9fb94d86c89f8bd0117e43f027be2437e360e50e
Arsen Abalyan <arsen.abalyan@jetrockets.ru>
2019-07-17 16:51:35 +0300

    Added login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
 end
