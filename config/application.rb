17c5885b85cc453a37fcfd968d9bd11a0ed8da7b
Arsen Abalyan <arsen.abalyan@jetrockets.ru>
2019-07-17 17:44:51 +0300

    Added logout and current_user(for ability check: user is logged in)

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,16 @@ module TodoList
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    helper_method :current_user
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def require_user
+      redirect_to '/login' unless current_user
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   resources :users
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
