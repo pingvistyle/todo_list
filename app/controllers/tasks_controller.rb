86b7fb2fdb248835bc495f05e1629c89490a66ca
Arsen Abalyan <arsen.abalyan@jetrockets.ru>
2019-07-19 16:53:54 +0300

    Added CRUD for tasks, styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,10 +10,46 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_path, notice: 'Task created!' }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if resource_task.update(task_params)
+        format.html { redirect_to tasks_path, notice: 'Update completed' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    resource_task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_path, notice: 'Task destroyed!' }
+    end
+  end
+
   protected
 
   def resource_task
-    @task = Task.find(params[:id])
+    @task = params[:id].present? ? Task.find(params[:id]) : Task.new
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :completed, :due_date, :description)
   end
 
 end
